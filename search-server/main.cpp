5584316fd44757014c22b9dedfbf49463619e50c
SciWake <SciWake@gmail.com>
2023-08-18 21:43:27 +0600

    Move functions to the server class
    FindAllDocuments, FindTopDocuments, MatchDocument and ParseQuery functions

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-/*
+
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
-*/
+
 
 string ReadLine() {
     string s;
@@ -44,7 +44,7 @@ vector<string> SplitIntoWords(const string& text) {
     return words;
 }
 
-/*
+
 struct Document {
     int id;
     int relevance;
@@ -54,6 +54,7 @@ bool HasDocumentGreaterRelevance(const Document& lhs, const Document& rhs) {
     return lhs.relevance > rhs.relevance;
 }
 
+/*
 set<string> ParseQuery(const string& text, const set<string>& stop_words) {
     set<string> query_words;
     for (const string& word : SplitIntoWordsNoStop(text, stop_words)) {
@@ -116,6 +117,16 @@ public:
         documents_.push_back({document_id, words});
     }
 
+    vector<Document> FindTopDocuments(const string& raw_query) {
+        auto matched_documents = FindAllDocuments(ParseQuery(raw_query));
+
+        sort(matched_documents.begin(), matched_documents.end(), HasDocumentGreaterRelevance);
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+    }
+
 private:
     struct DocumentContent {
         int id = 0;
@@ -139,6 +150,41 @@ private:
         }
         return words;
     }
+
+    static int MatchDocument(const DocumentContent& content, const set<string>& query_words) {
+        if (query_words.empty()) {
+            return 0;
+        }
+        set<string> matched_words;
+        for (const string& word : content.words) {
+            if (matched_words.count(word) != 0) {
+                continue;
+            }
+            if (query_words.count(word) != 0) {
+                matched_words.insert(word);
+            }
+        }
+        return static_cast<int>(matched_words.size());
+    }
+
+    vector<Document> FindAllDocuments(const set<string>& query_words) {
+        vector<Document> matched_documents;
+        for (const auto& document : documents_) {
+            const int relevance = MatchDocument(document, query_words);
+            if (relevance > 0) {
+                matched_documents.push_back({document.id, relevance});
+            }
+        }
+        return matched_documents;
+    }
+
+    set<string> ParseQuery(const string& text) {
+        set<string> query_words;
+        for (const string& word : SplitIntoWordsNoStop(text)) {
+            query_words.insert(word);
+        }
+        return query_words;
+    }
 };
 
 SearchServer CreateSearchServer() {
@@ -155,6 +201,11 @@ SearchServer CreateSearchServer() {
 
 int main() {
     SearchServer search_server = CreateSearchServer();
+    const string query = ReadLine();
+    for (auto [document_id, relevance] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = "s << document_id << ", relevance = "s << relevance << " }"s
+             << endl;
+    }
 }
 
 /*
