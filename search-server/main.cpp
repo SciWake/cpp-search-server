59a5fb860a72b0c11b12096eb031f53be07f4cb0
SciWake <SciWake@gmail.com>
2023-09-12 06:05:36 +0600

    Deleted duplicate code

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -236,13 +236,6 @@ void PrintDocument(const Document& document) {
     cout << "{ "s
          << "document_id = "s << document.id << ", "s
          << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating << " }"s << endl;
-}
-
-void PrintDocument(const Document& document) {
-    cout << "{ "s
-         << "document_id = "s << document.id << ", "s
-         << "relevance = "s << document.relevance << ", "s
          << "rating = "s << document.rating
          << " }"s << endl;
 }
