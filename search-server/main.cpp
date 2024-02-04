f3995adb65ef2d84fe5fa8ed1ecc30026f09fd68
SciWake <sciwake@gmail.com>
2024-02-05 02:34:38 +0600

    Update the code interaction interface

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -376,22 +376,25 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
         cout << "Ошибка совпадения документов на запрос "s << query << ": "s << e.what() << endl;
     }
 }
- 
- 
+
 int main() {
-    SearchServer search_server("и в на%"s);
+    SearchServer search_server("и в на"s);
 
-    // Добавление документов
-    AddDocument(search_server, 1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 8, 2, 3, 4 });
-    AddDocument(search_server, 2, "пушистый пес и модный ошейник"s, DocumentStatus::ACTUAL, { 1, 2, 3, 4 });
-    AddDocument(search_server, 3, "большой пушистый кот большой пес"s, DocumentStatus::ACTUAL, { 1, 2, 3, 4, 5 });
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "большой кот модный ошейник "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "большой пёс скворец василий"s, DocumentStatus::ACTUAL, {1, 1, 1});
 
-    // Поиск документов
-    FindTopDocuments(search_server, "пушистый -пес"s);
-    FindTopDocuments(search_server, "пушистый кот"s);
-    FindTopDocuments(search_server, "большой пес"s);
+    const auto search_results = search_server.FindTopDocuments("пушистый пёс"s);
+    int page_size = 2;
+    const auto pages = Paginate(search_results, page_size);
 
-    // Сопоставление документов с запросом
-    MatchDocuments(search_server, "пушистый пес"s);
-    MatchDocuments(search_server, "кот -пушистый"s);
+    // Выводим найденные документы по страницам
+    for (auto page = pages.begin(); page != pages.end(); ++page) {
+        cout << *page << endl;
+        cout << "Разрыв страницы"s << endl;
+    }
+
+    return 0;
 }
