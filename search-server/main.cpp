3433c480b907b7b40469e474f9c465f9165dd093
SciWake <sciwake@gmail.com>
2024-02-05 02:39:22 +0600

    Add IteratorRange class

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -371,6 +371,32 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
     }
 }
 
+template <typename Iterator>
+class IteratorRange {
+public:
+    IteratorRange(Iterator begin, Iterator end)
+        : first_(begin)
+        , last_(end)
+        , size_(distance(first_, last_)) {
+    }
+
+    Iterator begin() const {
+        return first_;
+    }
+
+    Iterator end() const {
+        return last_;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+private:
+    Iterator first_, last_;
+    size_t size_;
+};
+
 int main() {
     SearchServer search_server("и в на"s);
 
