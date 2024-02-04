6ae68db066e8602ed56cb546b01bed2fbb4b3b20
SciWake <sciwake@gmail.com>
2024-02-05 02:44:16 +0600

    Fix code style Document structure

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -50,13 +50,16 @@ vector<string> SplitIntoWords(const string& text) {
 
 struct Document {
     Document() = default;
-    Document(int id, 
-             double relevance, int rating): id(id), 
-             relevance(relevance), 
-             rating(rating) {}
-             int id = 0;
-             double relevance = 0.0;
-             int rating = 0;
+
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
 };
  
 template <typename StringContainer>
