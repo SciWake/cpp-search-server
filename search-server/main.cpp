ee81e317bdbca0bff372269474a4e5aa587b9690
SciWake <sciwake@gmail.com>
2024-02-05 02:40:35 +0600

    Implement Paginator class

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -380,6 +380,36 @@ ostream& operator<<(ostream& out, const IteratorRange<Iterator>& range) {
 }
 
 template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (size_t left = distance(begin, end); left > 0;) {
+            const size_t current_page_size = min(page_size, left);
+            const Iterator current_page_end = next(begin, current_page_size);
+            pages_.push_back({begin, current_page_end});
+
+            left -= current_page_size;
+            begin = current_page_end;
+        }
+    }
+
+    auto begin() const {
+        return pages_.begin();
+    }
+
+    auto end() const {
+        return pages_.end();
+    }
+
+    size_t size() const {
+        return pages_.size();
+    }
+
+private:
+    vector<IteratorRange<Iterator>> pages_;
+};
+
+template <typename Iterator>
 class IteratorRange {
 public:
     IteratorRange(Iterator begin, Iterator end)
