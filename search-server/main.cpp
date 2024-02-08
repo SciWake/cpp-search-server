1ed62b28e01c4d6122ba577e2d0818361aaf91e4
SciWake <sciwake@gmail.com>
2024-02-09 02:38:39 +1200

    Implement the AddRequest method

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -457,7 +457,20 @@ private:
     const static int min_in_day_ = 1440;
 
     void AddRequest(int results_num) {
-
+        // новый запрос - новая секунда
+        ++current_time_;
+        // удаляем все результаты поиска, которые устарели
+        while (!requests_.empty() && min_in_day_ <= current_time_ - requests_.front().timestamp) {
+            if (0 == requests_.front().results) {
+                --no_results_requests_;
+            }
+            requests_.pop_front();
+        }
+        // сохраняем новый результат поиска
+        requests_.push_back({current_time_, results_num});
+        if (0 == results_num) {
+            ++no_results_requests_;
+        }
     }
 };
 
