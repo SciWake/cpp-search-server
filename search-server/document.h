bf3c0ed21a609c04fa9950f266eed77277f3a48f
SciWake <sciwake@gmail.com>
2024-02-15 02:58:22 +0600

    Comment out unused code

--- search-server/document.h
+++ search-server/document.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
-
-#include "search_server.h"
-
-
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
 struct Document {
     Document() = default;
@@ -21,13 +15,13 @@ enum class DocumentStatus {
     BANNED,
     REMOVED,
 };
-std::ostream& operator<<(std::ostream& out, const Document& document);
-void PrintDocument(const Document& document);
-void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status);
-void AddDocument(SearchServer& search_server, 
-                 int document_id, 
-                 const std::string& document, 
-                 DocumentStatus status,
-                 const std::vector<int>& ratings);
-void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
-void MatchDocuments(const SearchServer& search_server, const std::string& query);
+// std::ostream& operator<<(std::ostream& out, const Document& document);
+// void PrintDocument(const Document& document);
+// void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status);
+// void AddDocument(SearchServer& search_server, 
+//                  int document_id, 
+//                  const std::string& document, 
+//                  DocumentStatus status,
+//                  const std::vector<int>& ratings);
+// void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
+// void MatchDocuments(const SearchServer& search_server, const std::string& query);
